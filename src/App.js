ab73c20fa5f379bb65ab8931cf7d41f2b0e65cae
unknown <sean.herbison@gmail.com>
2023-05-28 18:53:17 -0700

    Make a product card and puts it in the body of the page.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from "react";
 import styles from "./App.module.css"; // Using modular CSS for this project
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import ProductCard from './Components/ProductCard/ProductCard';// For testing, will go in CardDisplay later
+
 
 function App() {
   const [displayedProducts, setDisplayedProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+
 
   const fetchData = () => {
     fetch("./data.json")
       .then((response) => response.json())
       .then((data) => {
         let sortedProducts = [...data.products];
-        console.log(sortedProducts); // Makes sure data is being passed correctly.
+        // console.log(sortedProducts); // Makes sure data is being passed correctly.
+        setProducts(sortedProducts);
       });
   };
 
@@ -27,6 +32,8 @@ function App() {
         <h1>Stuff</h1>
         <h1>Stuff</h1>
         <h1>Stuff</h1>
+        {products.length > 0 && <ProductCard product={products[0]} />} {/* Render first product */}
+
       </main>
       <Footer />
     </div>
--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,1 +1,34 @@
-//npm install --save react-star-ratings
+import React from "react";
+import styles from "./ProductCard.module.css";
+import StarRatings from 'react-star-ratings'; //npm install --save react-star-ratings
+
+const ProductCard = ({ product }) => {
+  const { title, brand, category, stock, rating, images, price } = product;
+  // add description for hover
+
+  return (
+    <div className={styles.productCard}>
+      <img src={images[0]} alt={title} />
+      {/* Takes first image. Replace with Carousel. */}
+      <div className={styles.center}>
+        <h3>{title}</h3>
+        <p>{brand}</p>
+        <p>{category}</p>
+        <p className={stock < 20 ? styles.lowStock : ""}>{stock} in stock</p>
+        <p>${price}</p>
+        <div className={styles.rating}>
+          <StarRatings
+            rating={Math.round(rating * 2) / 2} // Rounds to nearest half for star display
+            numberOfStars={5}
+            starDimension="20px"
+            starSpacing="2px"
+            starRatedColor="gold"
+          />
+          <p>({rating.toFixed(1)})</p> {/* Displays numerical rating to tenth */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
