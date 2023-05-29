501ca98a0c8df0bf52108bc12b576b59be95e1c0
unknown <sean.herbison@gmail.com>
2023-05-28 21:25:14 -0700

    Adds sorting logic for CategorySelect.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Footer from "./Components/Footer/Footer";
 import CardDisplay from "./Components/CardDisplay/CardDisplay";
 import ControlBar from "./Components/ControlBar/ControlBar";
 
-
 function App() {
   const [displayedProducts, setDisplayedProducts] = useState([]);
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState("");
   const [brands, setBrands] = useState([]);
-
+  const [sort, setSort] = useState("name");
+  const [order, setOrder] = useState("asc");
+  const [search, setSearch] = useState("");
 
   const fetchData = () => {
     fetch("./data.json")
@@ -19,13 +20,20 @@ function App() {
       .then((data) => {
         let sortedProducts = [...data.products];
         // console.log(sortedProducts); // Makes sure data is being passed correctly.
+
+        // Filter products by category, if one is selected
+        if (category !== "") {
+          sortedProducts = sortedProducts.filter(
+            (product) => product.category === category
+          );
+        }
         setProducts(sortedProducts);
       });
   };
 
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
+  }, [category, sort, order, search, fetchData]);
 
   useEffect(() => {
     setDisplayedProducts(products);
@@ -36,9 +44,15 @@ function App() {
       <Header />
       <main className={styles.main}>
         <ControlBar
-            category={category}
-            setCategory={setCategory}
-          />
+          category={category}
+          setCategory={setCategory}
+          sort={sort}
+          setSort={setSort}
+          order={order}
+          setOrder={setOrder}
+          brands={brands}
+          setBrands={setBrands}
+        />
         <CardDisplay products={displayedProducts} />
       </main>
       <Footer />
