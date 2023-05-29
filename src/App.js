36654b7b9e44dad7dd5d58e595d69c1f3193a948
unknown <sean.herbison@gmail.com>
2023-05-28 18:33:15 -0700

    Moved data.json to public folder to allow calling with fetch. Styled App to take whole page, with footer at bottom.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import{ useState } from 'react';
+import React, { useState, useEffect } from "react";
 import styles from "./App.module.css"; // Using modular CSS for this project
-import data from "./data/data.json";
 import Header from "./Components/Header/Header";
-import Footer from './Components/Footer/Footer';
-
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   const [displayedProducts, setDisplayedProducts] = useState([]);
 
+  const fetchData = () => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let sortedProducts = [...data.products];
+        console.log(sortedProducts); // Makes sure data is being passed correctly.
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
-    <div>
+    <div className={styles.app}>
       <Header />
-      <Footer /> {/* Currently hidden behind fixed header */}
+      <main className={styles.main}>
+        <h1>Stuff</h1>
+        <h1>Stuff</h1>
+        <h1>Stuff</h1>
+        <h1>Stuff</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
